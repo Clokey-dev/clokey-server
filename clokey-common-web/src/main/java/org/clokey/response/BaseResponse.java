e520e0879b951325e5ab21f0eb3a820e63fc6848
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-07-30 04:35:41 +0900

    refactor: BaseResponse record화 및 timeStamp 추가

--- clokey-common-web/src/main/java/org/clokey/response/BaseResponse.java
+++ clokey-common-web/src/main/java/org/clokey/response/BaseResponse.java
@@ -3,36 +3,36 @@ package org.clokey.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import java.time.LocalDateTime;
 import org.clokey.code.BaseSuccessCode;
 import org.clokey.exception.BaseErrorCode;
 
-@Getter
-@AllArgsConstructor
-@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
-public class BaseResponse<T> {
-
-    @JsonProperty("isSuccess")
-    private final Boolean isSuccess;
-
-    private final String code;
-    private final String message;
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private T result;
-
+@JsonPropertyOrder({"isSuccess", "code", "message", "timeStamp", "result"})
+public record BaseResponse<T>(
+        @JsonProperty("isSuccess") Boolean isSuccess,
+        String code,
+        String message,
+        LocalDateTime timeStamp,
+        @JsonInclude(JsonInclude.Include.NON_NULL) T result) {
     public static <T> BaseResponse<T> onSuccess(BaseSuccessCode code, T result) {
         return new BaseResponse<>(
-                true, code.getReasonDto().code(), code.getReasonDto().message(), result);
+                true,
+                code.getReasonDto().code(),
+                code.getReasonDto().message(),
+                LocalDateTime.now(),
+                result);
     }
 
     public static <T> BaseResponse<T> onFailure(BaseErrorCode code, T result) {
         return new BaseResponse<>(
-                false, code.getErrorReason().code(), code.getErrorReason().message(), result);
+                false,
+                code.getErrorReason().code(),
+                code.getErrorReason().message(),
+                LocalDateTime.now(),
+                result);
     }
 
     public static <T> BaseResponse<T> onFailure(String code, String message, T data) {
-        return new BaseResponse<>(false, code, message, data);
+        return new BaseResponse<>(false, code, message, LocalDateTime.now(), data);
     }
 }
