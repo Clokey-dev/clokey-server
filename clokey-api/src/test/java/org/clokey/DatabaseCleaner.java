6922c70aeb956929e438f95b5d5853133e251f2b
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-07-31 14:41:18 +0900

    [feat/#14] DB Cleaner 도입 및 Integration 상속 추상 클래스 구현

---
+++ clokey-api/src/test/java/org/clokey/DatabaseCleaner.java
@@ -0,0 +1,62 @@
+package org.clokey;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import org.hibernate.Session;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseCleaner implements InitializingBean {
+
+    @PersistenceContext private EntityManager entityManager;
+
+    private List<String> tableNames;
+
+    @Override
+    public void afterPropertiesSet() {
+        entityManager.unwrap(Session.class).doWork(this::extractTableNames);
+    }
+
+    private void extractTableNames(Connection conn) {
+        tableNames =
+                entityManager.getMetamodel().getEntities().stream()
+                        .map(e -> e.getName().replaceAll("([a-z])([A-Z])", "$1_$2").toLowerCase())
+                        .toList();
+    }
+
+    public void execute() {
+        entityManager.unwrap(Session.class).doWork(this::cleanTables);
+    }
+
+    private void cleanTables(Connection conn) throws SQLException {
+        Statement statement = conn.createStatement();
+        statement.executeUpdate("SET REFERENTIAL_INTEGRITY FALSE");
+
+        for (String name : tableNames) {
+            statement.executeUpdate(String.format("TRUNCATE TABLE %s", name));
+            if (columnExists(conn, name, name)) {
+                statement.executeUpdate(
+                        String.format(
+                                "ALTER TABLE %s ALTER COLUMN %s_id RESTART WITH 1", name, name));
+            }
+        }
+
+        statement.executeUpdate("SET REFERENTIAL_INTEGRITY TRUE");
+    }
+
+    private boolean columnExists(Connection conn, String tableName, String columnName)
+            throws SQLException {
+        try (ResultSet rs =
+                conn.getMetaData()
+                        .getColumns(
+                                null, null, tableName.toUpperCase(), columnName.toUpperCase())) {
+            return rs.next();
+        }
+    }
+}
