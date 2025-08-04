c414196685cafb833f0b64ee98a461b1fdf8ca42
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-08-04 21:12:22 +0900

    refactor: like 도메인 리팩터링

--- clokey-domain/src/main/java/org/clokey/like/entity/MemberLike.java
+++ clokey-domain/src/main/java/org/clokey/like/entity/MemberLike.java
@@ -9,16 +9,6 @@ import org.clokey.member.entity.Member;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(
-        uniqueConstraints = {
-            @UniqueConstraint(
-                    name = "uk_member_history",
-                    columnNames = {"member_id", "history_id"})
-        },
-        indexes = {
-            @Index(name = "idx_member_like_member_id", columnList = "member_id"),
-            @Index(name = "idx_member_like_history_id", columnList = "history_id")
-        })
 public class MemberLike extends BaseEntity {
 
     @Id
@@ -33,13 +23,13 @@ public class MemberLike extends BaseEntity {
     @JoinColumn(name = "history_id", nullable = false)
     private History history;
 
-    @Builder(access = AccessLevel.PRIVATE)
-    private MemberLike(Member member, History history) {
-        this.member = member;
-        this.history = history;
-    }
-
-    public static MemberLike createMemberLike(Member member, History history) {
-        return MemberLike.builder().member(member).history(history).build();
-    }
+    //    @Builder(access = AccessLevel.PRIVATE)
+    //    private MemberLike(Member member, History history) {
+    //        this.member = member;
+    //        this.history = history;
+    //    }
+    //
+    //    public static MemberLike createMemberLike(Member member, History history) {
+    //        return MemberLike.builder().member(member).history(history).build();
+    //    }
 }
