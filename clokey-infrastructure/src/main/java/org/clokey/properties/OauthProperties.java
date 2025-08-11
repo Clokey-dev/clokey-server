868f4d0888340113426aae8f50115563cc6d0d8e
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-08-11 14:20:21 +0900

    feat: OauthProperties 추가

---
+++ clokey-infrastructure/src/main/java/org/clokey/properties/OauthProperties.java
@@ -0,0 +1,21 @@
+package org.clokey.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "oidc")
+public record OauthProperties(Kakao kakao, Apple apple) {
+    public record Kakao(
+            String clientId,
+            String clientSecret,
+            String redirectUri,
+            String appId,
+            String adminKey) {}
+
+    public record Apple(
+            String teamId,
+            String keyId,
+            String clientId,
+            String redirectUri,
+            String privateKey,
+            String issuer) {}
+}
