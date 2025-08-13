0836637141fc011e0bc9839a6b61766a1e72d780
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-08-14 04:30:31 +0900

    refactor: 대댓글 존재 여부 추가

--- clokey-api/src/main/java/org/clokey/domain/comment/repository/CommentRepositoryImpl.java
+++ clokey-api/src/main/java/org/clokey/domain/comment/repository/CommentRepositoryImpl.java
@@ -1,12 +1,16 @@
 package org.clokey.domain.comment.repository;
 
 import static org.clokey.comment.entitiy.QComment.comment;
+import static org.clokey.comment.entitiy.QReply.reply;
 import static org.clokey.member.entity.QMember.member;
 
+import com.querydsl.core.group.GroupBy;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.clokey.domain.comment.dto.response.CommentListResponse;
 import org.clokey.global.paging.SortDirection;
@@ -24,6 +28,8 @@ public class CommentRepositoryImpl implements CommentRepositoryCustom {
     @Override
     public Slice<CommentListResponse> findAllByHistoryId(
             Long historyId, Long lastCommentId, int size, SortDirection direction) {
+
+        // 삼중 조인을 피하기 위해 댓글 존재 여부는 처음에는 false로 가져옵니다.
         List<CommentListResponse> results =
                 queryFactory
                         .select(
@@ -33,11 +39,12 @@ public class CommentRepositoryImpl implements CommentRepositoryCustom {
                                         member.id,
                                         member.nickname,
                                         member.profileImageUrl,
-                                        comment.content))
+                                        comment.content,
+                                        Expressions.constant(false)))
                         .from(comment)
                         .join(comment.member, member)
                         .where(
-                                comment.member.id.eq(member.id),
+                                comment.history.id.eq(historyId),
                                 lastCommentIdCondition(lastCommentId, direction))
                         .orderBy(
                                 direction == SortDirection.DESC
@@ -46,7 +53,41 @@ public class CommentRepositoryImpl implements CommentRepositoryCustom {
                         .limit(size + 1)
                         .fetch();
 
-        return checkLastPage(size, results);
+        boolean hasNext = results.size() > size;
+        if (hasNext) {
+            results = results.subList(0, size);
+        }
+
+        Map<Long, Boolean> repliedMap =
+                results.isEmpty()
+                        ? Map.of()
+                        : queryFactory
+                                .select(reply.comment.id)
+                                .from(reply)
+                                .where(
+                                        reply.comment.id.in(
+                                                results.stream()
+                                                        .map(CommentListResponse::commentId)
+                                                        .toList()))
+                                .groupBy(reply.comment.id)
+                                .transform(
+                                        GroupBy.groupBy(reply.comment.id)
+                                                .as(Expressions.constant(true)));
+
+        List<CommentListResponse> finalResults =
+                results.stream()
+                        .map(
+                                c ->
+                                        new CommentListResponse(
+                                                c.commentId(),
+                                                c.memberId(),
+                                                c.nickName(),
+                                                c.profileImageUrl(),
+                                                c.content(),
+                                                repliedMap.getOrDefault(c.commentId(), false)))
+                        .toList();
+
+        return checkLastPage(size, finalResults);
     }
 
     private BooleanExpression lastCommentIdCondition(Long commentId, SortDirection direction) {
