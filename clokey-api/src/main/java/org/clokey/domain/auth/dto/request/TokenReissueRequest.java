378a947cc8286447609856ead58d2d00b6115a94
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-08-23 22:05:24 +0900

    chore: 오타 정정

--- clokey-api/src/main/java/org/clokey/domain/auth/dto/request/TokenReissueRequest.java
+++ clokey-api/src/main/java/org/clokey/domain/auth/dto/request/TokenReissueRequest.java
@@ -4,5 +4,5 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record TokenReissueRequest(
-        @NotBlank(message = "리프레시 토큰은 비워둘 수 없습니다.") @Schema(description = "토큰 재발급을 위한 리프래시 토큰")
+        @NotBlank(message = "리프레시 토큰은 비워둘 수 없습니다.") @Schema(description = "토큰 재발급을 위한 리프레시 토큰")
                 String refreshToken) {}
