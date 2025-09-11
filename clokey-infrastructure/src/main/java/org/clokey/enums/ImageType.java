b372eb52030a2ce771c8399c93cd7fc433a5aec2
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-09-11 17:31:58 +0900

    [fix/#58] 오타 정정

--- clokey-infrastructure/src/main/java/org/clokey/enums/ImageType.java
+++ clokey-infrastructure/src/main/java/org/clokey/enums/ImageType.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum ImageType {
     MEMBER_PROFILE("member-profile", "회원 프로필 사진"),
     MEMBER_BACKGROUND("member-background", "회원 배경 사진"),
-    ClOTH_IMAGE("cloth-image", "옷 사진"),
+    CLOTH_IMAGE("cloth-image", "옷 사진"),
     HISTORY_IMAGE("history-image", "기록 사진");
 
     private final String type;
