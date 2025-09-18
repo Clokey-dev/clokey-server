8e8edb0db4b4367ff864d3c348333ee910f7343e
leedy5521 <leedy5521@naver.com>
2025-09-18 14:35:13 +0900

    delete: 기존 엔티티 및 레포지토리 삭제

--- clokey-api/src/main/java/org/clokey/domain/report/repository/CommentReportRepository.java
+++
@@ -1,6 +0,0 @@
-package org.clokey.domain.report.repository;
-
-import org.clokey.report.entity.CommentReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentReportRepository extends JpaRepository<CommentReport, Long> {}
--- clokey-api/src/main/java/org/clokey/domain/report/repository/ProfileReportRepository.java
+++
@@ -1,6 +0,0 @@
-package org.clokey.domain.report.repository;
-
-import org.clokey.report.entity.ProfileReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileReportRepository extends JpaRepository<ProfileReport, Long> {}
