c05992cb5c41c72565138f72af2b340f20ae3ad6
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2025-11-11 00:19:23 +0900

    refactor: errorcode 수정

--- clokey-api/src/main/java/org/clokey/domain/history/exception/StyleErrorCode.java
+++ clokey-api/src/main/java/org/clokey/domain/history/exception/StyleErrorCode.java
@@ -8,8 +8,8 @@ import org.clokey.exception.BaseErrorCode;
 @Getter
 @AllArgsConstructor
 public enum StyleErrorCode implements BaseErrorCode {
-    INVALID_STYLE_COUNT(400, "STYLE_4001", "스타일은 1개 이상 3개 이하만 선택 가능합니다."),
-    STYLE_NOT_FOUND(404, "STYLE_4041", "존재하지 않는 스타일입니다.");
+    STYLE_NOT_FOUND(404, "STYLE_4041", "존재하지 않는 스타일입니다."),
+    DUPLICATED_STYLE(400, "STYLE_4001", "중복된 스타일이 존재합니다.");
     private final int status;
     private final String code;
     private final String message;
