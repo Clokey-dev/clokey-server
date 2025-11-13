ac3168f0c189257cb44ce7b1b340542f6122f405
leedy5521 <leedy5521@naver.com>
2025-11-13 23:29:31 +0900

    test: firebaseMessaging mocking

--- clokey-api/src/test/java/org/clokey/ClokeyApiApplicationTests.java
+++ clokey-api/src/test/java/org/clokey/ClokeyApiApplicationTests.java
@@ -1,12 +1,16 @@
 package org.clokey;
 
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ClokeyApiApplicationTests {
+
+    @MockitoBean private FirebaseMessaging mockFirebaseMessaging;
 
     @Test
     void contextLoads() {}
