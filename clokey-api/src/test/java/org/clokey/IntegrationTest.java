9b833ce8d1a646dce6a232f63d11e3127672af57
leedy5521 <leedy5521@naver.com>
2025-11-13 22:35:37 +0900

    test: 팔로우 푸시 알림 테스트 작성

--- clokey-api/src/test/java/org/clokey/IntegrationTest.java
+++ clokey-api/src/test/java/org/clokey/IntegrationTest.java
@@ -1,15 +1,18 @@
 package org.clokey;
 
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 public abstract class IntegrationTest {
 
     @Autowired protected DatabaseCleaner databaseCleaner;
+    @MockitoBean private FirebaseMessaging mockFirebaseMessaging;
 
     @BeforeEach
     void setUp() {
