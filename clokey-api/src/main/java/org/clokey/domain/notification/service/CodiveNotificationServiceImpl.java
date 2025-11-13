858abe753f10a3e891ed3fbaa8158c6199829e23
leedy5521 <leedy5521@naver.com>
2025-11-13 21:52:22 +0900

    feat: 비공개/공개 팔로우 알림 분리 구현(notiification service)

--- clokey-api/src/main/java/org/clokey/domain/notification/service/CodiveNotificationServiceImpl.java
+++ clokey-api/src/main/java/org/clokey/domain/notification/service/CodiveNotificationServiceImpl.java
@@ -7,7 +7,6 @@ import com.google.firebase.messaging.Notification;
 import lombok.RequiredArgsConstructor;
 import org.clokey.domain.member.exception.MemberErrorCode;
 import org.clokey.domain.member.repository.MemberRepository;
-import org.clokey.domain.notification.dto.response.NewFollowerNotificationResponse;
 import org.clokey.domain.notification.exception.NotificationErrorCode;
 import org.clokey.domain.notification.repository.CodiveNotificationRepository;
 import org.clokey.domain.term.enums.TermInfo;
@@ -30,10 +29,10 @@ public class CodiveNotificationServiceImpl implements CodiveNotificationService 
     private final FirebaseMessaging firebaseMessaging;
 
     private static final String NEW_FOLLOWER_NOTIFICATION = "%s님이 회원님의 옷장을 팔로우하기 시작했습니다.";
+    private static final String NEW_PENDING_FOLLOWER_NOTIFICATION = "%s님이 회원님의 옷장에 팔로우를 요청했습니다.";
 
     @Override
-    public NewFollowerNotificationResponse sendNewFollowerNotification(
-            Long followFromId, Long followToId) {
+    public void sendNewFollowerNotification(Long followFromId, Long followToId) {
         Member followFromMember = getMemberById(followFromId);
         Member followToMember = getMemberById(followToId);
 
@@ -49,6 +48,9 @@ public class CodiveNotificationServiceImpl implements CodiveNotificationService 
                     Message.builder()
                             .setToken(followToMember.getDeviceToken())
                             .setNotification(notification)
+                            .putData("followFromId", String.valueOf(followFromMember.getId()))
+                            .putData("clokeyId", followFromMember.getClokeyId())
+                            .putData("redirectType", "MEMBER_PROFILE")
                             .build();
 
             try {
@@ -66,12 +68,48 @@ public class CodiveNotificationServiceImpl implements CodiveNotificationService 
                             RedirectType.MEMBER_REDIRECT);
 
             codiveNotificationRepository.save(codiveNotification);
-
-            return new NewFollowerNotificationResponse(
-                    content, profileImageUrl, followFromId, followFromMember.getClokeyId());
         }
+    }
 
-        return null;
+    @Override
+    public void sendNewPendingFollowerNotification(Long followFromId, Long followToId) {
+        Member followFromMember = getMemberById(followFromId);
+        Member followToMember = getMemberById(followToId);
+
+        if (isAbleToSendNotification(followToMember)) {
+            String content =
+                    String.format(
+                            NEW_PENDING_FOLLOWER_NOTIFICATION, followFromMember.getNickname());
+            String profileImageUrl = followFromMember.getProfileImageUrl();
+
+            Notification notification =
+                    Notification.builder().setBody(content).setImage(profileImageUrl).build();
+
+            Message message =
+                    Message.builder()
+                            .setToken(followToMember.getDeviceToken())
+                            .setNotification(notification)
+                            .putData("followFromId", String.valueOf(followFromMember.getId()))
+                            .putData("clokeyId", followFromMember.getClokeyId())
+                            .putData("redirectType", "MEMBER_PROFILE")
+                            .build();
+
+            try {
+                firebaseMessaging.send(message);
+            } catch (FirebaseMessagingException e) {
+                throw new BaseCustomException(NotificationErrorCode.NOTIFICATION_FIREBASE_ERROR);
+            }
+
+            CodiveNotification codiveNotification =
+                    CodiveNotification.createCodiveNotification(
+                            followFromMember,
+                            content,
+                            profileImageUrl,
+                            followFromMember.getClokeyId(),
+                            RedirectType.MEMBER_REDIRECT);
+
+            codiveNotificationRepository.save(codiveNotification);
+        }
     }
 
     private Member getMemberById(Long memberId) {
