823728ab7cf71cdead62ecea46c7b26167bb6cd4
leedy5521 <leedy5521@naver.com>
2025-11-20 00:22:10 +0900

    test/fix: 테스트 수정

--- clokey-api/src/test/java/org/clokey/domain/member/service/MemberServiceTest.java
+++ clokey-api/src/test/java/org/clokey/domain/member/service/MemberServiceTest.java
@@ -640,9 +640,11 @@ class MemberServiceTest extends IntegrationTest {
             Follow follow23 = Follow.createFollow(member2, member3);
             Follow follow31 = Follow.createFollow(member3, member1);
             Follow follow32 = Follow.createFollow(member3, member2);
+            Follow follow42 = Follow.createFollow(member4, member2);
             Block block = Block.createBlock(member3, member1);
 
-            followRepository.saveAll(List.of(follow12, follow13, follow23, follow31, follow32));
+            followRepository.saveAll(
+                    List.of(follow12, follow13, follow23, follow31, follow32, follow42));
             blockRepository.save(block);
         }
 
@@ -654,7 +656,7 @@ class MemberServiceTest extends IntegrationTest {
             // then
             assertThat(response.content())
                     .extracting("codiveId", "isMe")
-                    .containsExactly(tuple("testCodiveId3", false), tuple("testCodiveId2", false));
+                    .containsExactly(tuple("testCodiveId2", false));
         }
 
         @Test
@@ -663,7 +665,9 @@ class MemberServiceTest extends IntegrationTest {
             SliceResponse<FollowMemberResponse> response =
                     memberService.getFollows(2L, null, false, 10);
             // then
-            assertThat(response.content()).extracting("codiveId").containsExactly("testCodiveId1");
+            assertThat(response.content())
+                    .extracting("codiveId")
+                    .containsExactly("testCodiveId4", "testCodiveId1");
         }
 
         @Test
@@ -685,7 +689,7 @@ class MemberServiceTest extends IntegrationTest {
             // then
             assertThat(response.content())
                     .extracting("codiveId", "isMe")
-                    .containsExactly(tuple("testCodiveId1", true));
+                    .containsExactly(tuple("testCodiveId4", false), tuple("testCodiveId1", true));
         }
 
         @Test
