5fc27ef4416c162c2d6060b5c85804674d599427
leedy5521 <leedy5521@naver.com>
2025-11-21 00:02:49 +0900

    fix: 변수명 수정

--- clokey-api/src/main/java/org/clokey/domain/notification/dto/response/UnreadNotificationResponse.java
+++ clokey-api/src/main/java/org/clokey/domain/notification/dto/response/UnreadNotificationResponse.java
@@ -3,4 +3,5 @@ package org.clokey.domain.notification.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record UnreadNotificationResponse(
-        @Schema(description = "안읽음 알림의 존재 유무", example = "true") boolean isUnreadNotification) {}
+        @Schema(description = "안읽음 알림의 존재 유무", example = "true")
+                boolean existsUnreadNotification) {}
