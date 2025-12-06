3c4aebe2a779b182c23bb268c1dace4c3147121c
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-12-06 18:10:32 +0900

    fix: cloth 테스트 실패 수정

--- clokey-api/src/main/java/org/clokey/domain/cloth/service/ClothServiceImpl.java
+++ clokey-api/src/main/java/org/clokey/domain/cloth/service/ClothServiceImpl.java
@@ -159,11 +159,9 @@ public class ClothServiceImpl implements ClothService {
 
         validateClothOwnership(cloth, currentMember.getId());
 
-        coordinateClothRepository.deleteAllInBatch(
-                coordinateClothRepository.findAllByClothId(clothId));
-        clothFolderRepository.deleteAllInBatch(clothFolderRepository.findAllByClothId(clothId));
-        historyClothTagRepository.deleteAllInBatch(
-                historyClothTagRepository.findAllByClothId(clothId));
+        coordinateClothRepository.deleteAllByClothId(cloth.getId());
+        clothFolderRepository.deleteAllByClothId(cloth.getId());
+        historyClothTagRepository.deleteAllByClothId(cloth.getId());
 
         eventPublisher.publishEvent(ImageDeleteEvent.of(cloth.getClothImageUrl()));
         clothRepository.delete(cloth);
