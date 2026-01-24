459a679d6c0dd406e5dc0975f53f104ad42ef97e
나용준 <141994188+youngJun99@users.noreply.github.com>
2026-01-25 03:10:53 +0900

    fix: 테스트 설정

--- clokey-batch/src/test/java/org/clokey/ClokeyBatchApplicationTests.java
+++ clokey-batch/src/test/java/org/clokey/ClokeyBatchApplicationTests.java
@@ -2,8 +2,10 @@ package org.clokey;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ClokeyBatchApplicationTests {
 
     @Test
